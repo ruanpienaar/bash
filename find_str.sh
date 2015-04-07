43749f7b8af451d868eafb3e5a67b0a9c9cdad37
ruanpienaar <ruan800@gmail.com>
2015-04-07 15:28:38 +0100

    ..

--- find_str.sh
+++ find_str.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#function usage {                                                                                        │        at fitnesse.responders.ChunkingResponder$RespondingRunnable.run(ChunkingResponder.java:106)
-#  echo "usage: ./find_str.sh [PATH] [Searching string]"                                                                     │        at java.lang.Thread.run(Thread.java:722)
-#} 
+function usage {
+  echo "find_str.sh [PATH] [FILENAME]"
+}
 
 if [ -n "$1" ] && [ -n "$2" ]; then
   echo "Looking for \"$2\" in $1"
   find $1 -name "*.erl" -type f | xargs grep $2 --color=always
 else  
-#  usage
-   echo "mmm"
+  usage
 fi
