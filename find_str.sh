905eadde432cd2b2deabbf902196942c5e23ff86
ruanpienaar <ruan800@gmail.com>
2015-04-07 15:23:08 +0100

    added script to find a STR in a PATH

--- find_str.sh
+++ find_str.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-if [ -n "$1" ] && [ -n "$2" ]; then 
-  find . -name "*.erl" -type f | xargs grep $1
-else  
-  usage
-fi
 
-function usage{
-  echo "usage: ./find_str.sh [STR]"
-}
+#function usage {                                                                                        │        at fitnesse.responders.ChunkingResponder$RespondingRunnable.run(ChunkingResponder.java:106)
+#  echo "usage: ./find_str.sh [PATH] [Searching string]"                                                                     │        at java.lang.Thread.run(Thread.java:722)
+#} 
+
+if [ -n "$1" ] && [ -n "$2" ]; then
+  echo "Looking for \"$2\" in $1"
+  find $1 -name "*.erl" -type f | xargs grep $2 --color=always
+else  
+#  usage
+   echo "mmm"
+fi
